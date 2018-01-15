58bd9d78f0026ad014b14882b906f7bc094e8a00
Rodrigo Rodrigues de Aquino <rodrigorcomp@gmail.com>
2018-01-15 14:47:13 -0200

    add script led / remove Tabs

--- lib/gpio.rb
+++ lib/gpio.rb
@@ -1,31 +1,31 @@
 SYSFS_GPIO_DIR = "/sys/class/gpio"
 
 class GPIO
-	def self.export(gpio_number)
-		File.open(SYSFS_GPIO_DIR + '/export', 'w') do |file|
-			file.write(gpio_number)
-		end
-	end
+  def self.export(gpio_number)
+    File.open(SYSFS_GPIO_DIR + '/export', 'w') do |file|
+      file.write(gpio_number)
+    end
+  end
 
-	def self.gpio_get_value(gpio_number)
-		value_file_path = SYSFS_GPIO_DIR + "/gpio#{gpio_number}/value"
-		File.read(value_file_path)
-	end
+  def self.gpio_get_value(gpio_number)
+    value_file_path = SYSFS_GPIO_DIR + "/gpio#{gpio_number}/value"
+    File.read(value_file_path)
+  end
 
-	def self.gpio_set_value(gpio_number, value)
-		value_file_path = SYSFS_GPIO_DIR + "/gpio#{gpio_number}/value"
+  def self.gpio_set_value(gpio_number, value)
+    value_file_path = SYSFS_GPIO_DIR + "/gpio#{gpio_number}/value"
 
-		File.open(value_file_path, 'w') do |file|
-			file.write(value)
-		end
-	end
+    File.open(value_file_path, 'w') do |file|
+      file.write(value)
+    end
+  end
 
-	def self.gpio_set_direction(gpio_number, direction)
-		direction_file_path = SYSFS_GPIO_DIR + "/gpio#{gpio_number}/direction"
+  def self.gpio_set_direction(gpio_number, direction)
+    direction_file_path = SYSFS_GPIO_DIR + "/gpio#{gpio_number}/direction"
 
-		File.open(direction_file_path, 'w') do |file|
-			file.write(direction)
-		end
-	end
+    File.open(direction_file_path, 'w') do |file|
+      file.write(direction)
+    end
+  end
 end
 
--- main.rb
+++ main.rb
@@ -6,30 +6,39 @@ GATE_GPIO = 60
 GATE_LED_GPIO = 67
 NET_ACCESS_LED = 48
 
-gate = 						Gate.new(GATE_GPIO)
-gate_led = 				Led.new(GATE_LED_GPIO)
-connection_led = 	Led.new(NET_ACCESS_LED)
-network = 				Network.new
-count = 					0
+gate =            Gate.new(GATE_GPIO)
+gate_led =        Led.new(GATE_LED_GPIO)
+connection_led =  Led.new(NET_ACCESS_LED)
+script_led =      Led.new(NET_ACCESS_LED)
+network =         Network.new
+count =           0
 
 loop do
-	gate.update_status
+  gate.update_status
 
-	if gate.open?
+  if gate.open?
     gate_led.turn_on
   else
-		gate_led.turn_off
-	end
+    gate_led.turn_off
+  end
 
-	if count.zero?
-		if network.connected?
-			connection_led.turn_on
-		else
-			connection_led.turn_off
-		end
-	end
+  if count.zero?
+    if network.connected?
+      connection_led.turn_on
+    else
+      connection_led.turn_off
+    end
+  end
 
-	sleep 0.1
-	count += 1
-	count = 0 if count == 100
+  if count % 10 == 0
+    if script_led.on?
+      script_led.turn_off
+    else
+      script_led.turn_on
+    end
+  end
+
+  sleep 0.1
+  count += 1
+  count = 0 if count == 100
 end
